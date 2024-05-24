b1f24e8c53393ff2d23b1114d80b04e86d3e2aa7
Aadrika-Shukla <aadrikashukla19@gmail.com>
2024-05-24 22:26:11 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -29,10 +29,23 @@ class Topicform(forms.ModelForm):
 class Webpageform(forms.ModelForm):
     class Meta:
         model=WebPage
-        fields='__all__'
+        fields='__all__'        # to create input fields for all the coloumns in out models
+        #fields=['topic_name','name']           # to create specific input fields it will display column name in the order you pass
+        #exclude=['email']                        # to create input field except the column name we specified
+        #labels={'topic_name':'TOPIC NAME','name':'NAME'}       # by default label will be our coloumn name we declare in our models in ourder to give another lable name other than our coloumn name use lables in form of dictionary{'colomn name':'new label name'}
+        #widgets={'url':forms.PasswordInput,'name':forms.Textarea()}     # by default passowrd ,text area every field will be created as charfield only because we don't have any specific input type=password or textarea so we use here widgets to transform our charfield to password field or textarea
+        #widgets={'url':forms.PasswordInput,'name':forms.Textarea(attrs={'rows':3,'cols':3})}    # to customize text area block we give attrs attribute where values will be in form of dictionary
+
 
 
 class Accessform(forms.ModelForm):
     class Meta:
         model=AccessRecord
-        fields='__all__'
+        fields='__all__'         # to create input fields for all the coloumns in out models
+        #fields=['author','name']           # to create specific input fields it will display column name in the order you pass
+        #exclude=['author']                        # to create input field except the column name we specified
+        #labels={'date':'DATE','name':'NAME'}       # by default label will be our coloumn name we declare in our models in ourder to give another lable name other than our coloumn name use lables in form of dictionary{'colomn name':'new label name'}
+        #widgets={'name':forms.Textarea()}     # by default passowrd ,text area every field will be created as charfield only because we don't have any specific input type=password or textarea so we use here widgets to transform our charfield to password field or textarea
+        #widgets={'date':forms.PasswordInput,'name':forms.Textarea(attrs={'rows':3,'cols':3})}    # to customize text area block we give attrs attribute where values will be in form of dictionary
+
+        
